06351f18e787b3e880f493835818fd180478d2f4
majunting <e0008325@u.nus.edu>
2022-03-08 18:01:41 +0800

    add login template

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
+# # another straightaway method to add configurations (not recommended)
+# app.config['SECRET_KEY'] = 'secret_key'
 
 from app import routes
--- flask/app/routes.py
+++ flask/app/routes.py
@@ -1,6 +1,37 @@
+from flask import render_template, flash, redirect
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return 'Hello world'
+	user = {'username': 'user1'}
+	posts = [
+		{
+			'author': {'username': 'user2'},
+			'body': 'Post 1'
+		},
+		{
+			'author': {'username': 'user3'},
+			'body': 'Post2'
+		}
+	]
+	return render_template('index.html', title = 'home', user = user, posts = posts)
+# 	return '''
+# <html>
+#     <head>
+#         <title>Home Page - Microblog</title>
+#     </head>
+#     <body>
+#         <h1>Hello, ''' + user['username'] + '''!</h1>
+#     </body>
+# </html>'''
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for user {}, remember_me = {}'.format(
+			form.username.data, form.remember_me.data))
+		return redirect('/index')
+	return render_template('login.html', title = 'Sign In', form = form)
