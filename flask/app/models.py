95bdf4d23767d60dee41aad24cb3da5911bb162d
majunting <e0008325@u.nus.edu>
2022-03-16 18:06:43 +0800

    add 'follower' table to db

--- flask/app/models.py
+++ flask/app/models.py
@@ -6,6 +6,11 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from hashlib import md5
 
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64), index = True, unique = True)
@@ -15,6 +20,12 @@ class User(UserMixin, db.Model):
 
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default = datetime.utcnow)
+
+    followed = db.relationship(
+        'User', secondary = followers,
+        primaryjoin = (followers.c.follower_id == id),
+        secondaryjoin = (followers.c.followed_id == id),
+        backref = db.backref('followers', lazy = 'dynamic'), lazy = 'dynamic')
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
